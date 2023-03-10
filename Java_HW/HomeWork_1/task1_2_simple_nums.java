6143115cf4c257c8b3e8c97db64804dd49cae46e
Denis <mironov_dens@mail.ru>
2023-03-10 20:14:33 +0900

    Add decision of HomeWork_1 / task1_2.java

---
+++ Java_HW/HomeWork_1/task1_2_simple_nums.java
@@ -0,0 +1,20 @@
+/*
+ * Вывести все простые числа от 1 до 1000
+ */
+
+public class task1_2 {
+    public static void main(String[] args) {
+        System.out.println("Вывести все простые числа от 1 до 1000");
+        for (int i = 1; i <= 1000; i++) {
+            int count = 0;
+            for (int value = 1; value < 1000; value++) {
+                if (i % value == 0){
+                    count +=1;
+                }
+            }
+            if (count < 3) {
+                System.out.print("  " + i + ",");
+            }
+        }
+    }
+}
